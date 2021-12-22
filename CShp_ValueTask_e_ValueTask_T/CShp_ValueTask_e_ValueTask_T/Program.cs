678aaac9b6614f73510e43097e1b8abd35aeece2
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-22 11:58:31 -0300

    CShp_ValueTask_e_ValueTask_T

---
+++ CShp_ValueTask_e_ValueTask_T/CShp_ValueTask_e_ValueTask_T/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CShp_ValueTask_e_ValueTask_T
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await MetodoSemRetornoDeValor();
+            Console.WriteLine("A Tarefa foi concluida!");
+
+            var result = await MetodoRetorndoValor();
+            Console.WriteLine($"Valor retornado {result}");
+        }
+
+        static async ValueTask MetodoSemRetornoDeValor()
+        {
+            Console.WriteLine("MetodoSemRetornoDeValor");
+            await Task.Delay(2000);
+        }
+
+        static async ValueTask<int> MetodoRetorndoValor()
+        {
+            Console.WriteLine("MetodoRetorndoValor");
+            await Task.Delay(2000);
+            return 2022;
+        }
+
+    }
+}
---
+++ CShp_ValueTask_e_ValueTask_T/ValueTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ValueTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = CalculaSoma(0, 0).Result;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+
+        static async ValueTask<int> CalculaSoma(int a, int b)
+        {
+            if(a == 0 && b == 0)
+            {
+                return 0;
+            }
+
+            return await Task.Run(() => a + b);
+        }
+    }
+}
